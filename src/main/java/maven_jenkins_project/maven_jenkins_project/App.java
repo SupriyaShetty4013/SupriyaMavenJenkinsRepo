0c822aad89eeb6892fafc1753f84efe01b678624
Supriya123 <supriya123@gmail.com>
2024-09-06 21:23:10 +0530

    inital commit from maven project to local repo

---
+++ src/main/java/maven_jenkins_project/maven_jenkins_project/App.java
@@ -0,0 +1,13 @@
+package maven_jenkins_project.maven_jenkins_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
